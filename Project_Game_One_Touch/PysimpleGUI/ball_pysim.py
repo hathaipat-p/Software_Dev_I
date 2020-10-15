4aee12b6bdf6e39dc7b39480ed44323608936cb3
hathaipat-p <68021060+hathaipat-p@users.noreply.github.com>
2020-10-15 23:21:43 +0700

    Update ball_pysim.py

--- Project_Game_One_Touch/PysimpleGUI/ball_pysim.py
+++ Project_Game_One_Touch/PysimpleGUI/ball_pysim.py
@@ -6,24 +6,32 @@ from colors import *
 class ball_pysim():
 
     def __init__(self, canvas ):
-        position_ball = [ (50,50) , (200,50) , (350,50) , 
+        position_ball = [   (50,60) , (200,60) , (350,60) , 
                             (50,200) , (350,200) , 
                             (50,350) , (200,350) , (350,350)]
         r = random.randint(0,255)
         g = random.randint(0,255)
         b = random.randint(0,255)
 
-        self.r = 20
+        self.r = 50
         self.color = random.choice(COLORS)
         self.x, self.y = random.choice(position_ball) 
         self.canvas = canvas
-        #self.circle = self.canvas.DrawCircle((self.x, self.y), self.r, fill_color=self.color,line_color='black')
 
     def delete_ball(self):
         self.canvas.DeleteFigure(self.circle)
+
+    def is_collided(self, facex, facey ):
+        #if self.x-self.r < facex and facex < self.x+self.r and self.y-self.r < facey and facey < self.y+self.r:
+        #    return True
+        #else : return False
+
+        if self.x == 50 and self.y == 60 :
+            if 0 <= facex and facex <= 100 and 0 <= facey and facey <= 100:
+                return True
+        
+        else : return False
+
         
 
-    def is_collided(self,mx, my):
-        if self.draw.collidepoint((mx, my)):
-            return True
-        else : return False
+
